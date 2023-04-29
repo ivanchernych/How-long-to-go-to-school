cae3bdbfe3a423b52c78fe4a2cf517b1f0305f24
d1zzy <d1zzzy.project@gmail.com>
2023-04-29 21:48:26 +0300

    ready task 5

--- main.py
+++ main.py
@@ -1,63 +1,48 @@
-from getting import getting
+from func import lonlat_distance
 import requests
 from io import BytesIO
 from PIL import Image
 
-address = input()
+coord = input().split(';')
+coord_new = []
+for el in coord:
+    a, b = el.split(', ')
+    coord_new.append((float(a), float(b)))
+coord = coord_new
 
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+x = len(coord) // 2
 
+res = 0
 
-geocoder_params = {
-    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-    "geocode": address,
-    "format": "json"}
+for el in range(len(coord)):
+    if coord[el] == coord[-1]:
+        break
 
-response = requests.get(geocoder_api_server, params=geocoder_params)
-print(response.url)
+    res += int(lonlat_distance(coord[el], coord[el + 1]))
 
-tompony = getting(response)
-
-search_api_server = "https://search-maps.yandex.ru/v1/"
-api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
-
-address_ll = ','.join(tompony)
-
-search_params = {
-    "apikey": api_key,
-    "text": "метро",
-    "lang": "ru_RU",
-    "ll": address_ll,
-    "type": "biz"
-}
-
-response = requests.get(search_api_server, params=search_params)
-
-json_response = response.json()
-
-organization = json_response["features"][0]
-
-org_name = organization["properties"]["CompanyMetaData"]["name"]
-
-print(org_name)
-
-org_address = organization["properties"]["CompanyMetaData"]["address"]
-
-point = organization["geometry"]["coordinates"]
-
-org_point = "{0},{1}".format(point[0], point[1])
 delta = "0.009"
+pl = ''
+
+for i in range(len(coord)):
+    hh = "{0},{1}".format(coord[i][0], coord[i][1])
+    pl += f'{hh},'
+
+
+ad = "{0},{1}".format(coord[x][0], coord[x][1])
+
 
 map_params = {
-    "ll": address_ll,
+    "ll": ad,
     "spn": ",".join([delta, delta]),
     "l": "map",
-    "pt": f'{org_point},pm2dgl~{address_ll},pm2dgl',
-    "pl": f'{org_point},{address_ll}'
+    "pt": f'{ad},pm2dgl',
+    "pl": pl[:-1]
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 response = requests.get(map_api_server, params=map_params)
+
+print(res, 'м')
 
 Image.open(BytesIO(
     response.content)).show()
